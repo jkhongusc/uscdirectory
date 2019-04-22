dd1e9608f9a42dab0f931176edbbf7a725193b52
EC2 Default User <ec2-user@ip-10-254-0-37.us-west-2.aws.usc.edu>
2019-04-22 17:00:38 +0000

    auto upload to s3

--- create_zip.sh
+++ create_zip.sh
@@ -4,3 +4,10 @@
 zip -qr onlinedir.zip node_modules
 zip -qg onlinedir.zip main.js
 
+# upload automatically to s3
+aws s3 cp onlinedir.zip s3://tgw-us-west-2-public/onlinedir.zip
+
+# print s3 url for admin to use in AWS console
+echo "s3://tgw-us-west-2-public/onlinedir.zip"
+
+
